ced5612d575431601654883495a4398c3c53c2d6
[Anitha Alluri] <[anithareddyalluri@gmail.com]>
2024-07-30 13:46:02 -0400

    developed screen two

--- lib/screens/home/views/home_screen.dart
+++ lib/screens/home/views/home_screen.dart
@@ -1,32 +1,53 @@
 import 'dart:math';
 import 'dart:ui';
+import 'package:banking_group03/screens/stats/stats.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'main_screen.dart';
 
-class HomeScreen extends StatelessWidget{
+
+class HomeScreen extends StatefulWidget{
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int index=0;
+  late Color selectedItem = Colors.blue;
+  Color unselectedItem = Colors.grey;
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(),
       bottomNavigationBar: ClipRRect(
         borderRadius: const BorderRadius.vertical(
           top: Radius.circular(30)
         ),
         child: BottomNavigationBar(
-          backgroundColor: Colors.white,
+          onTap: (value){
+            setState(() {
+              index = value;
+            });
+          },
           showSelectedLabels: false,
           showUnselectedLabels: false,
           elevation: 3,
-          items: const[
+          items: [
           BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.home),
+              icon: Icon(
+                  CupertinoIcons.home,
+                   color: index == 0 ? selectedItem : unselectedItem
+              ),
           label: 'Home'
           ),
           BottomNavigationBarItem(
-              icon: Icon(CupertinoIcons.graph_square_fill),
+              icon: Icon(
+                  CupertinoIcons.graph_square_fill,
+                color: index == 1 ? selectedItem : unselectedItem
+              ),
           label: 'Stats'
           )
         ]
@@ -55,7 +76,9 @@ class HomeScreen extends StatelessWidget{
           ),
         ),
       ),
-      body: MainScreen()
+      body: index == 0
+        ? MainScreen()
+          :StatScreen()
     );
   }
 }
